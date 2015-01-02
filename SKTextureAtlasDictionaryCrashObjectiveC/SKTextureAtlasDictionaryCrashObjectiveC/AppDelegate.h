81f2e74ffc41a7ac1c3bda849d72bb3a53d0a144
Maciej Konieczny <hello@narf.pl>
2015-01-02 13:51:40 +0100

    added new universal Objective-C SpriteKit project

---
+++ SKTextureAtlasDictionaryCrashObjectiveC/SKTextureAtlasDictionaryCrashObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKTextureAtlasDictionaryCrashObjectiveC
+//
+//  Created by Maciej Konieczny on 2015-01-02.
+//  Copyright (c) 2015 Maciej Konieczny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKTextureAtlasDictionaryCrashObjectiveC/SKTextureAtlasDictionaryCrashObjectiveC/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SKTextureAtlasDictionaryCrashObjectiveC
+//
+
+//  Copyright (c) 2015 Maciej Konieczny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
